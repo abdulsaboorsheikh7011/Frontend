a94b265ae687eb36242ff1fb3ba33e9f8ec1cb46
abdulsaboorsheikh7011 <saboor7011@gmail.com>
2025-07-05 15:47:31 +0500

    My front end project

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <Navbar />
+    <main class="container">
+      <router-view />
+    </main>
+  </div>
+</template>
+
+<script>
+import Navbar from './components/NavBar.vue';
+
+export default {
+  name: 'AppRoot',
+  components: {
+    Navbar
+  }
+}
+</script>
+
+<style>
+body {
+  margin: 0;
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  background-color: #f0f2f5;
+}
+
+.container {
+  padding: 20px;
+}
+</style>
